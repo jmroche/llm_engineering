08c0bcd46e19a796b17b58a5d0567bad071b8759
Jose Roche <35976575+jmroche@users.noreply.github.com>
2024-12-17 02:43:57 +0000

    Completed Day 5 - Company Brochure.

--- week1/day5_exercise.py
+++ week1/day5_exercise.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from openai import OpenAI
+import json
 
 
 
@@ -36,20 +37,21 @@ class Website:
         #         self.links.append(href)
         site_info= {
             "title": self.title,
-            # "text": self.text,
+            "text": self.text,
             "links": self.links
         }
         return site_info
     
     @staticmethod
     def user_prompt_for_links_check(website):
-        system_prompt = "You are an assistant that helps creates company brochures. You need to analyze \
+        system_prompt = "You are an assistant that helps creates company pitch deck. You need to analyze \
                         a list of links passed to you from a website and decide which of these are useful links \
-                        to add in a brochure about the company. There are links not useful, like privacy policy \
-                        term of services, emails, etc.  \
-                        You will output the links as json as fully qualified urls. Some of the links \
+                        to add in a pitch deck about the company. There are links not useful, like privacy policy \
+                        term of services, emails, social media links, rss links, etc.  \
+                        You will output the links as json as fully qualified urls. Just output the raw json object, do not prefix it with ```json \
+                        Some of the links \
                         maight be relative links. Please follow the the follwing \
-                        structure for your resposne"
+                        structure for your resposne. "
         
         links =  """{
                         "links": [
@@ -72,6 +74,28 @@ class Website:
 
         return message
     
+
+    @staticmethod
+    def prompt_for_brochure(company_name, website_links):
+        system_prompt = f"""You are an assistant that helps creates company pitch decks. You will be given
+                            a website with a list of links and a summary of each link. You will create a pitch deck about the company 
+                            to prospective customers, investors and recruits. Respond in markdown. Include details about the ciompany 
+                            culture, customers and careers/jobs opportunities, if you have the information."""
+        
+        user_prompt = f"""You are looking at company {company_name}. Here are the contents of it's landing page and other relevant pages.
+                           Use this infomration to build a short pitch deck. The information of the relevant pages will be provided in a list of json objects format.
+                           Each object will have its page name as the key and the page text as its value.
+                           Website links: {website_links}"""
+
+        message = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+
+        ]
+
+        return message
+             
+    
     @staticmethod
     def get_site_links(website):
         response = requests.get(website)
@@ -86,7 +110,7 @@ class Website:
         website_data = self.scrape_website()
         response = openai.chat.completions.create(
             model="gpt-4o-mini",
-            messages=Website.user_prompt_for(self),
+            messages=Website.user_prompt_for_links_check(self),
             response_format={ "type": "json_object" },
 
         )
@@ -103,7 +127,7 @@ class Website:
     
     
     def link_checker(self, url):
-        self.uel = url
+        self.url = url
         website_data = self.scrape_website()
         response = openai.chat.completions.create(
             model="gpt-4o-mini",
@@ -111,6 +135,36 @@ class Website:
         )
         return response.choices[0].message.content
     
+    def make_pitch_deck(self):
+        response = openai.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=Website.prompt_for_brochure(self.company_name, self.website_details)
+        )
+
+        return response.choices[0].message.content
+    
+    
+    def get_site_mapping(self, company_name):
+        self.company_name = company_name
+        links = json.loads(self.link_checker(self.url))
+        self.website_details = []
+        # print(links)
+        for link in links["links"]:
+            # print(link["type"], link["link"])
+            website = Website(link["link"]).scrape_website()
+            # print(website["title"])
+            self.website_details.append(
+                {
+                    f"{link["type"]}": website["text"],
+                }
+            )
+            pitch_deck = self.make_pitch_deck()
+
+        return pitch_deck
+
+       
+
+        
     
     @classmethod
     def check_links(cls, url):
@@ -124,11 +178,17 @@ class Website:
 
 
 # summary = Website.display_sumamry("https://aws.amazon.com/blogs/aws/top-announcements-of-aws-reinvent-2024/")
+# print(summary)
 
-# links = Website.get_site_links("https://aws.amazon.com/blogs/aws/top-announcements-of-aws-reinvent-2024/")
+# links = Website.get_site_links("https://anthropic.com")
 
-company_website = Website.check_links("https://www.primerahora.com")
+# company_website = json.loads(Website.check_links("https://anthropic.com"))
+
 
 # company_website = Website.get_site_links("https://anthropic.com")
 
-print(company_website)
+# site = Website("https://anthropic.com/company")
+# company_website = site.get_site_links()
+
+site = Website("https://anthropic.com")
+print(site.get_site_mapping(company_name="Anthropic"))
